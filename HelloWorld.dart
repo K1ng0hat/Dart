9dd43d59434eddb201499d226980a3bf1b7de83b
Adrian Molano <trickshotsword@hotmail.com>
2024-05-24 14:18:59 -0500

    Agregando funciones

--- HelloWorld.dart
+++ HelloWorld.dart
@@ -4,20 +4,47 @@ void main() {
   String mySecondString = 'Caremonda al cuadrado';
 
   print (myString + 'picha monda y verga ' + mySecondString);
-  var myVariable = 'careverga';
+  var myVariable = true;
   var mySecondVariable = 25;
   var myThirdVariable = true;
   const myConstant = 'Constring';
   final int x = 10;
   final String y;
+  final myFinalConstant = myString;
 
-  y = 'Hola'; // Esto es permitido porque 'y' se asigna por primera vez aquí
+  //y = 'Hola'; // Esto es permitido porque 'y' se asigna por primera vez aquí
   //y = 'Mundo';  // Esto causaría un error porque 'y' ya está asignada
 
-  print(x); // Salida: 10
-  print(y); // Salida: Hola
+  //print(x); // Salida: 10
+  //print(y); // Salida: Hola
+
+//if (y != 0) {
+  //print('Es 0');
+//}else{
+  ////  print('Es 1 Caremondaaaa');}
+
+if (!myVariable) {
+
+} else if (myString == 'Mi cadena' || mySecondString.isEmpty) {
+
+} else {
+
+}
+String? myNullableString;
+myNullableString = 'mi cadena de texto nulable';
+myNullableString = null;
+
+List<int> myIntList = [1, 5, 10];
+myIntList.forEach((element) {
+
+  });
+
+myFunc('AdrianPichon');
+}
+void myFunc(String name){
 
 
 }
+
 
 
