59d79bf456b8b2548f2d105bff95144d0b160329
Adrian Molano <trickshotsword@hotmail.com>
2024-05-24 12:00:33 -0500

    Variables y cambios

--- HelloWorld.dart
+++ HelloWorld.dart
@@ -1,3 +1,23 @@
-void main(){
+void main() {
   print('Hello World');
+  String myString = 'Caremonda';
+  String mySecondString = 'Caremonda al cuadrado';
+
+  print (myString + 'picha monda y verga ' + mySecondString);
+  var myVariable = 'careverga';
+  var mySecondVariable = 25;
+  var myThirdVariable = true;
+  const myConstant = 'Constring';
+  final int x = 10;
+  final String y;
+
+  y = 'Hola'; // Esto es permitido porque 'y' se asigna por primera vez aquí
+  //y = 'Mundo';  // Esto causaría un error porque 'y' ya está asignada
+
+  print(x); // Salida: 10
+  print(y); // Salida: Hola
+
+
 }
+
+
