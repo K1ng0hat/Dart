f025602064732b5ae481548c1f04c7753fddffc6
Adrian Molano <trickshotsword@hotmail.com>
2024-05-29 11:58:17 -0500

    Dart X Flutter adding commit

---
+++ cellapp/packets/bin/packets.dart
@@ -0,0 +1,5 @@
+import 'package:packets/packets.dart' as packets;
+
+void main(List<String> arguments) {
+  print('Hello world: ${packets.calculate()}!');
+}
---
+++ cellapp/packets/test/packets_test.dart
@@ -0,0 +1,8 @@
+import 'package:packets/packets.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
